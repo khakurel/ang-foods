905d8e3a16a25d393748f650af112d9151c3d872
Suraj Khakurel <suraj@khakurel.com>
2016-09-15 09:34:09 -0700

    Added Result table spec

--- src/common/result_table/result_table.spec.js
+++ src/common/result_table/result_table.spec.js
@@ -6,21 +6,68 @@ import Component from './result_table.component';
 import Template from './result_table.html';
 
 describe('ResultTable', () => {
-    let makeController;
 
     beforeEach(window.module(Module.name));  // eslint-disable-line
-    beforeEach(inject(() => {
-        makeController = () => {
-            return new Controller();
-        };
-    }));
+
 
     describe('Controller', () => {
+        let controller;
+        let $event = {
+            preventDefault: sinon.spy()
+        };
+        beforeEach(inject((searchService, $state) => {
+            controller = new Controller(searchService, $state);
+        }));
+
         it('has a name property', () => {
-            let controller = makeController();
             expect(controller).to.have.property('name');
         });
+
+        describe('#goToReport', function () {
+            it('should called $state go', function () {
+                sinon.stub(controller.$state, 'go');
+                controller.goToReport($event, {ndbno: '1'});
+                expect($event.preventDefault).to.have.been.called;
+                expect(controller.$state.go).to.have.been.calledWith('reports', {id: '1'});
+                controller.$state.go.restore();
+            });
+        });
+
+
+        describe('#addToFavorite', function () {
+            it('should called serivice addToFavorite', function () {
+                sinon.stub(controller.service, 'addToFavorite');
+                controller.addToFavorite($event, {ndbno: '1', name: 'test'});
+                expect($event.preventDefault).to.have.been.called;
+                expect(controller.service.addToFavorite).to.have.been.calledWith({ndbno: '1', name: 'test'});
+                controller.service.addToFavorite.restore();
+            });
+        });
+
+
+        describe('#removeFromFavorite', function () {
+            it('should called service removeFromFavorite', function () {
+                sinon.stub(controller.service, 'removeFromFavorite');
+                controller.removeFromFavorite($event, {ndbno: '1', name: 'test'});
+                expect($event.preventDefault).to.have.been.called;
+                expect(controller.service.removeFromFavorite).to.have.been.calledWith({ndbno: '1', name: 'test'});
+                controller.service.removeFromFavorite.restore();
+            });
+
+            it('should called onDelete', function () {
+                controller.onDelete = sinon.spy();
+                sinon.stub(controller.service, 'removeFromFavorite');
+                controller.removeFromFavorite($event, {ndbno: '1', name: 'test'});
+                expect($event.preventDefault).to.have.been.called;
+                expect(controller.service.removeFromFavorite).to.have.been.calledWith({ndbno: '1', name: 'test'});
+                expect(controller.onDelete).to.have.been.called;
+                controller.service.removeFromFavorite.restore();
+            });
+        });
+
+
     });
+
 
     describe('Component', () => {
         // component/directive specs
@@ -37,11 +84,12 @@ describe('ResultTable', () => {
 
     describe('Component: Render', function () {
 
-        let element, $element, scope;
+        let element, $element, rootScope, scope;
         beforeEach(inject(function ($rootScope, $compile) {
-            scope = $rootScope.$new();
+            rootScope = $rootScope.$new();
             element = angular.element('<result-table items="items"></result-table>');
-            element = $compile(element)(scope);
+            element = $compile(element)(rootScope);
+            scope = element.isolateScope();
             $element = $(element[0])
         }));
 
@@ -57,11 +105,11 @@ describe('ResultTable', () => {
 
         it('should render correct number of records', ()=> {
 
-            scope.items = [
+            scope.$ctrl.items = [
                 {offset: 0, ndbno: "1001", name: "test1", group: "Test", favorite: true},
                 {offset: 1, ndbno: "1002", name: "test2", group: "Test", favorite: false}
             ];
-            scope.$apply();
+            rootScope.$digest();
 
             expect($element.find('tbody > tr').length).to.equal(2);
 
@@ -86,18 +134,40 @@ describe('ResultTable', () => {
 
         });
 
-        xit('should trigger ctrl\'s goToReport clicking on view report', ()=> {
-
-            scope.items = [
-                {offset: 0, ndbno: "1001", name: "test1", group: "Test", favorite: true},
-            ];
-            scope.$ctrl = {
-                goToReport :  sinon.spy()
-            };
-            scope.$apply();
+        it('should trigger goToReport clicking on view report', ()=> {
+            const data = {offset: 0, ndbno: "1001", name: "test1", group: "Test", favorite: true};
+            scope.$ctrl.items = [data];
+            rootScope.$digest();
+            scope.$ctrl.goToReport = sinon.spy();
             expect($element.find('tbody > tr').length).to.equal(1);
             $element.find('a.view').get(0).click();
             expect(scope.$ctrl.goToReport).to.have.been.called;
+            expect(scope.$ctrl.goToReport.getCall(0).args[1]).to.deep.equal(data);
+        });
+
+
+        it('should trigger removeFromFavorite clicking on Remove from Favorite', ()=> {
+            const data = {offset: 0, ndbno: "1001", name: "test1", group: "Test", favorite: true};
+            scope.$ctrl.items = [data];
+            rootScope.$digest();
+            scope.$ctrl.removeFromFavorite = sinon.spy();
+            expect($element.find('tbody > tr').length).to.equal(1);
+            $element.find('a.remove').get(0).click();
+            expect(scope.$ctrl.removeFromFavorite).to.have.been.called;
+            expect(scope.$ctrl.removeFromFavorite.getCall(0).args[1]).to.deep.equal(data);
+        });
+
+
+        it('should trigger addToFavorite clicking on add to  Favorite', ()=> {
+            const data = {offset: 0, ndbno: "1001", name: "test1", group: "Test", favorite: false};
+            scope.$ctrl.items = [data];
+            rootScope.$digest();
+            scope.$ctrl.addToFavorite = sinon.spy();
+            expect($element.find('tbody > tr').length).to.equal(1);
+            $element.find('a.add').get(0).click();
+            expect(scope.$ctrl.addToFavorite).to.have.been.called;
+            expect(scope.$ctrl.addToFavorite.getCall(0).args[1]).to.deep.equal(data);
+
         })
 
     });
